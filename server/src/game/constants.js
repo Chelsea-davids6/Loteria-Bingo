81ec0cc25a93f4e074378d2c0b0261d55c3a7d3f
Chelsea <132057611+Chelsea-davids6@users.noreply.github.com>
2026-02-24 20:27:22 +0200

    Added file in the backend to placce all constants

---
+++ server/src/game/constants.js
@@ -0,0 +1,24 @@
+const GRID_SIZE = 4; 
+const TOTAL_CARDS = 16;
+
+const GRIDS = {
+    rowOne: ["#", "#", "#", "#"], 
+    rowTwo: ["#", "#", "#", "#"],
+    rowThree: ["#", "#", "#", "#"],
+    rowFour: ["#", "#", "#", "#"]
+  };
+  
+
+const ICONS = [
+    "bread", "apple", "avocado", "bacon", "boiled_egg", "canned_fish",
+    "carrot", "cheese", "chocolate", "coconut", "coke", "corn",
+    "cucumber", "egg_plant", "french_hot_dog", "hot_tea", "lemon",
+    "lollipop", "nut", "onion", "orange", "peach", "pear",
+    "popcorn"
+];
+
+module.exports = {
+  ICONS,
+  GRIDS,
+  TOTAL_CARDS,
+};
