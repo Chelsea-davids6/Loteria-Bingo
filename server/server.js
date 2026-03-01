12074f448eb9017dbd1b6e5b505003c1b4705781
Chelsea <132057611+Chelsea-davids6@users.noreply.github.com>
2026-03-01 20:47:16 +0200

    Added a end point for start game

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { generatePlayerCard } = require("./src/game/gameLogic");
 
 const app = express();
 app.use(cors());
@@ -7,6 +8,17 @@ app.use(express.json());
 
 app.get("/api", (req, res) => {
   res.json({ message: "Server is running!" });
+});
+
+app.post("/api/game/start", (req, res) => {
+  console.log("Starting new game");
+  
+  const playerCard = generatePlayerCard();
+  
+  res.json({
+    success: true,
+    playerCard: playerCard
+  });
 });
 
 app.listen(5000, () => {
