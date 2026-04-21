7919ff4ce751b5a2584eac309b6c97a4ab1b55c0
Chelsea <132057611+Chelsea-davids6@users.noreply.github.com>
2026-04-21 20:26:02 +0200

    Added game state

---
+++ server/src/game/gameState.js
@@ -0,0 +1,30 @@
+const { generatePlayerCard, generateRandomCall } = require('./gameLogic');
+
+const games = new Map();
+
+class GameState {
+  constructor(gameId) {
+    this.gameId = gameId;
+    this.playerCard = generatePlayerCard();
+    this.callingOrder = generateCallingOrder();
+    this.calledIcons = [];
+    this.currentCallIndex = 0;
+    this.isActive = true;
+    this.winner = null;
+    this.currentRound = 1; 
+    this.createdAt = new Date();
+ }
+
+  callNextIcon() {
+    if (this.currentCallIndex >= this.callingOrder.length) {
+      return null;
+    }
+
+    const nextIcon = this.callingOrder[this.currentCallIndex];
+    this.calledIcons.push(nextIcon);
+    this.currentCallIndex++;
+
+    return nextIcon;
+  }
+  
+}
